afd73c95ec78080573f7d0b5199c595a1fddaa24
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2011-10-22 12:37:09 +0000

    + unrolled versions
    git-svn-id: file:///mnt/data/SVNrepo@2357 a13746d7-5020-4d46-8679-245a07f888a7

--- ssse3_popcount.c
+++ ssse3_popcount.c
@@ -1,13 +1,15 @@
 /*
 	Bit population count, $Revision$
 
-	This program includes three functions:
+	This program includes following functions:
 	* lookup  --- lookup based 
 	* ssse3-1 --- SSSE3 using PSHUFB and PSADBW
 	* ssse3-2 --- improved SSSE3 procedure - PSADBW called fewer times
 	* sse2-1  --- bit-parallel counting and PSADBW
 	* sse2-2  --- bit-parallel counting - PSADBW called fewer times (the same
 	              optimization as in ssse3-2)
+	* ssse3-unrl --- ssse3-2 with inner loop unrolled 4 times
+	* sse2-unrl --- ssse2-2 with inner loop unrolled 4 times
 	
 	compilation:
 	$ gcc -O3 -Wall -pedantic -std=c99 ssse3_popcount.c
@@ -308,6 +310,91 @@ uint32_t sse2_popcount2(uint8_t* buffer, int chunks16) {
 	return result;
 }
 
+
+// ---- SSE2 - naive approach improved - inner loop unrolled --------------
+uint32_t sse2_popcount3(uint8_t* buffer, int chunks16) {
+	static uint8_t mask33[16] = {0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33}; 
+	static uint8_t mask55[16] = {0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55}; 
+	static uint8_t mask0f[16] = {0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f, 0x0f}; 
+
+	uint32_t result = 0;
+	int i, n, k;
+
+#ifdef DEBUG
+	assert(chunks % 4 == 0)
+#ifndef ALIGN_DATA
+	assert(false);
+#endif
+#endif
+
+	__asm__ volatile ("movdqu (%%eax), %%xmm5" : : "a" (mask55));
+	__asm__ volatile ("movdqu (%%eax), %%xmm6" : : "a" (mask33));
+	__asm__ volatile ("movdqu (%%eax), %%xmm7" : : "a" (mask0f));
+	__asm__ volatile ("pxor %xmm4, %xmm4");	// global accumulator
+
+	i = 0;
+	while (chunks16 > 0) {
+#define MAX (7*4)
+		if (chunks16 > MAX) {
+			k = MAX;
+			chunks16 -= MAX;
+		}
+		else {
+			k = chunks16;
+			chunks16 = 0;
+		}
+#undef MAX
+
+		__asm__ volatile ("pxor %xmm3, %xmm3"); // xmm3 -- local accumulator
+		for (n=0; n < k; n += 4) {
+#define block(index) \
+			__asm__ volatile( \
+				"movdqa	  (%%eax), %%xmm0	\n" \
+				"movdqa    %%xmm0, %%xmm1	\n" \
+				"psrlw         $1, %%xmm1	\n" \
+				"pand      %%xmm5, %%xmm0	\n" \
+				"pand      %%xmm5, %%xmm1	\n" \
+				"paddb     %%xmm1, %%xmm0	\n" \
+				"movdqa    %%xmm0, %%xmm1	\n" \
+				"psrlw         $2, %%xmm1	\n" \
+				"pand      %%xmm6, %%xmm0	\n" \
+				"pand      %%xmm6, %%xmm1	\n" \
+				"paddb     %%xmm1, %%xmm0	\n" \
+				"movdqa    %%xmm0, %%xmm1	\n" \
+				"psrlw         $4, %%xmm1	\n" \
+				"pand      %%xmm7, %%xmm0	\n" \
+				"pand      %%xmm7, %%xmm1	\n" \
+				"paddb     %%xmm1, %%xmm0	\n" \
+				"paddb     %%xmm0, %%xmm3	\n"	\
+				: : "a" (&buffer[index]));
+
+			block(i + 0*16);
+			block(i + 1*16);
+			block(i + 2*16);
+			block(i + 3*16);
+			i += 4*16;
+		}
+
+		// update global accumulator (two 32-bits counters)
+		__asm__ volatile (
+			"pxor	%xmm0, %xmm0		\n"
+			"psadbw	%xmm0, %xmm3		\n"
+			"paddd	%xmm3, %xmm4		\n"
+		);
+	}
+
+	// finally add together 32-bits counters stored in global accumulator
+	__asm__ volatile (
+		"movhlps   %%xmm4, %%xmm0	\n"
+		"paddd     %%xmm4, %%xmm0	\n"
+		"movd      %%xmm0, %%eax	\n"
+		: "=a" (result)
+	);
+
+	return result;
+}
+
+
 // ---- SSSE3 - naive approach --------------------------------------------
 uint32_t ssse3_popcount1(uint8_t* buffer, int chunks16) {
 	uint32_t dummy __attribute__((unused));
@@ -373,14 +460,16 @@ uint32_t ssse3_popcount2(uint8_t* buffer, int chunks16) {
 	while (chunks16 > 0) {
 		// max(POPCOUNT_8bit) = 8, thus byte-wise addition could be done
 		// for floor(255/8) = 31 iterations
-		if (chunks16 > 31) {
-			k = 31;
-			chunks16 -= 31;
+#define MAX (31)
+		if (chunks16 > MAX) {
+			k = MAX;
+			chunks16 -= MAX;
 		}
 		else {
 			k = chunks16;
 			chunks16 = 0;
 		}
+#undef MAX
 
 		__asm__ volatile ("pxor %xmm4, %xmm4"); // xmm4 -- local accumulator
 		for (n=0; n < k; n++) {
@@ -429,10 +518,95 @@ uint32_t ssse3_popcount2(uint8_t* buffer, int chunks16) {
 	return result;
 }
 
+// ---- SSSE3 - better alorithm, inner loop unrolled ----------------------
+uint32_t ssse3_popcount3(uint8_t* buffer, int chunks16) {
+	static char MASK_4bit[16] = {0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf};
 
-#define OPT_COUNT 6
+	uint32_t result;
 
-char* functions[OPT_COUNT] = {"verify", "lookup", "sse2-1", "sse2-2", "ssse3-1", "ssse3-2"};
+#ifdef DEBUG
+	assert(chunks16 % 4 == 0);
+#endif
+
+	__asm__ volatile ("movdqu (%%eax), %%xmm7" : : "a" (POPCOUNT_4bit));
+	__asm__ volatile ("movdqu (%%eax), %%xmm6" : : "a" (MASK_4bit));
+	__asm__ volatile ("pxor    %%xmm5, %%xmm5" : : ); // xmm5 -- global accumulator
+
+	result = 0;
+
+	int k, n, i;
+
+	i = 0;
+	while (chunks16 > 0) {
+		// max(POPCOUNT_8bit) = 8, thus byte-wise addition could be done
+		// for floor(255/8) = 31 iterations
+#define MAX (7*4)
+		if (chunks16 > MAX) {
+			k = MAX;
+			chunks16 -= MAX;
+		}
+		else {
+			k = chunks16;
+			chunks16 = 0;
+		}
+#undef MAX
+		__asm__ volatile ("pxor %xmm4, %xmm4"); // xmm4 -- local accumulator
+		for (n=0; n < k; n+=4) {
+#define body(index) \
+			__asm__ volatile( \
+				"movdqa	  (%%eax), %%xmm0	\n" \
+				"movdqa    %%xmm0, %%xmm1	\n" \
+				"psrlw         $4, %%xmm1	\n" \
+				"pand      %%xmm6, %%xmm0	\n" \
+				"pand      %%xmm6, %%xmm1	\n" \
+				"movdqa    %%xmm7, %%xmm2	\n" \
+				"movdqa    %%xmm7, %%xmm3	\n" \
+				"pshufb    %%xmm0, %%xmm2	\n" \
+				"pshufb    %%xmm1, %%xmm3	\n" \
+				"paddb     %%xmm2, %%xmm4	\n" \
+				"paddb     %%xmm3, %%xmm4	\n" \
+				: : "a" (&buffer[index]));
+
+			body(i);
+			body(i + 1*16);
+			body(i + 2*16);
+			body(i + 3*16);
+#undef body
+			i += 4*16;
+		}
+
+		// update global accumulator (two 32-bits counters)
+		__asm__ volatile (
+			"pxor	%xmm0, %xmm0		\n"
+			"psadbw	%xmm0, %xmm4		\n"
+			"paddd	%xmm4, %xmm5		\n"
+		);
+	}
+
+	// finally add together 32-bits counters stored in global accumulator
+	__asm__ volatile (
+		"movhlps   %%xmm5, %%xmm0	\n"
+		"paddd     %%xmm5, %%xmm0	\n"
+		"movd      %%xmm0, %%eax	\n"
+		: "=a" (result)
+	);
+
+	return result;
+}
+
+
+#define OPT_COUNT 8
+
+char* functions[OPT_COUNT] = {
+	"verify",
+	"lookup",
+	"sse2-1",
+	"sse2-2",
+	"ssse3-1",
+	"ssse3-2",
+	"ssse3-unrl",
+	"sse2-unrl"
+};
 
 
 void help(const char* progname) {
@@ -521,6 +695,8 @@ int main(int argc, char* argv[]) {
 			verify(3, sse2_popcount2);
 			verify(4, ssse3_popcount1);
 			verify(5, ssse3_popcount2);
+			verify(6, ssse3_popcount3);
+			verify(7, sse2_popcount3);
 
 			if (failed)
 				return EXIT_FAILURE;
@@ -550,6 +726,16 @@ int main(int argc, char* argv[]) {
 		case 5:
 			while (repeat_count--)
 				ssse3_popcount2(buffer, chunks_count);
+			break;
+
+		case 6:
+			while (repeat_count--)
+				ssse3_popcount3(buffer, chunks_count);
+			break;
+
+		case 7:
+			while (repeat_count--)
+				sse2_popcount3(buffer, chunks_count);
 			break;
 	}
 
