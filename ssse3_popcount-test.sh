5d4dc797fdeaf7b635fbcac30311e9d296de945d
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2011-10-22 12:38:04 +0000

    git-svn-id: file:///mnt/data/SVNrepo@2358 a13746d7-5020-4d46-8679-245a07f888a7

--- ssse3_popcount-test.sh
+++ ssse3_popcount-test.sh
@@ -1,4 +1,4 @@
-tmp=`tempfile`
+# usage: sh ssse3_popcount.sh 2> ssse3_popcount_speedup.data
 
 for size in 1 8; do
 	for proc in lookup "sse2-1" "sse2-2" "ssse3-1" "ssse3-2"; do
@@ -6,12 +6,12 @@ for size in 1 8; do
 	done
 done
 for size in 32; do
-	for proc in lookup "sse2-1" "sse2-2" "ssse3-1" "ssse3-2"; do
+	for proc in lookup "sse2-1" "sse2-2" "ssse3-1" "ssse3-2" "sse2-unrl" "ssse3-unrl"; do
 		/usr/bin/time -f "$proc $size %U" ./ssse3_popcount $proc $size 2000000
 	done
 done
 for size in 128 512 1024 2048; do
-	for proc in lookup "sse2-1" "sse2-2" "ssse3-1" "ssse3-2"; do
+	for proc in lookup "sse2-1" "sse2-2" "ssse3-1" "ssse3-2" "sse2-unrl" "ssse3-unrl"; do
 		/usr/bin/time -f "$proc $size %U" ./ssse3_popcount $proc $size 200000
 	done
 done
