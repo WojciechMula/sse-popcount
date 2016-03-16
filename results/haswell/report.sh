93aa64ce40b445d87cd33d35839c5fa5bdb50eeb
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-03-16 19:59:57 +0100

    +detailed results from Haswell

---
+++ results/haswell/report.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name=haswell-i7-4770-gcc5.3.0-avx2
+
+python ../../scripts/report.py \
+    --csv="$name.csv" \
+    --output="$name.rst" \
+    --cpu="Core i7-4770 CPU @ 3.40GHz" \
+    --architecture="AVX2" \
+    --compiler="GCC 5.3.0 (Ubuntu)" \
+    --runs=5
+
+if type rst2html > /dev/null
+then
+    echo "generating $name.html"
+    rst2html "$name.rst" > "$name.html"
+fi
