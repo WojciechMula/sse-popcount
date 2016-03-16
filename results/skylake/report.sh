85ef7a54a921a6571be137854e995f46b521853a
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-03-16 20:04:41 +0100

    +detailed results from Skylake

---
+++ results/skylake/report.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name=skylake-i7-6700-gcc5.3.0-avx2
+
+python ../../scripts/report.py \
+    --csv="$name.csv" \
+    --output="$name.rst" \
+    --cpu="Skylake i7-6700 CPU @ 3.40GHz" \
+    --architecture="AVX2" \
+    --compiler="GCC 5.3.0 (Ubuntu)" \
+    --runs=5
+
+if type rst2html > /dev/null
+then
+    echo "generating $name.html"
+    rst2html "$name.rst" > "$name.html"
+fi
