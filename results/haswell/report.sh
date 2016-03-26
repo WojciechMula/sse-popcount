cd4ef97e0b6fd8731d5e9bb8ca331fc4cc321a71
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-03-26 16:49:29 +0100

    added results for binaries produced by clang

--- results/haswell/report.sh
+++ results/haswell/report.sh
@@ -10,8 +10,21 @@ python ../../scripts/report.py \
     --compiler="GCC 5.3.0 (Ubuntu)" \
     --runs=5
 
+name=haswell-i7-4770-clang3.8.0-avx2
+
+python ../../scripts/report.py \
+    --csv="$name.csv" \
+    --output="$name.rst" \
+    --cpu="Haswell Core i7-4770 CPU @ 3.40GHz" \
+    --architecture="AVX2" \
+    --compiler="3.8.0-svn257311-1~exp1 (Ubuntu)" \
+    --runs=5
+
 if type rst2html > /dev/null
 then
-    echo "generating $name.html"
-    rst2html "$name.rst" > "$name.html"
+    for src in *.csv
+    do
+        dst=`basename $src .csv`.html
+        rst2html "$src" > "$dst"
+    done
 fi
