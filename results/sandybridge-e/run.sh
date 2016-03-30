bc5e2cbfabfb63d47b17121ebff4fd52214372d5
Julian Kent <julian.kent@pix4d.com>
2016-03-30 15:17:54 +0200

    Add SandyBridge-E results

---
+++ results/sandybridge-e/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+prog="../../speed_avx_g++-5 --csv"
+iters=5
+tmp=tmp.csv
+result=result.csv
+
+rm -f $tmp
+for i in `seq $iters`
+do
+    echo "run $i"
+    $prog   32 100000000 | tee -a $tmp
+    $prog   64  50000000 | tee -a $tmp
+    $prog  128  25000000 | tee -a $tmp
+    $prog  256  12500000 | tee -a $tmp
+    $prog  512  10000000 | tee -a $tmp
+    $prog 1024   5000000 | tee -a $tmp
+    $prog 2048   2500000 | tee -a $tmp
+    $prog 4096   1250000 | tee -a $tmp
+done
+
+mv $tmp $result
