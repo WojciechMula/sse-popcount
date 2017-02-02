2a583aa46baf8ed84f85e4f3559d9c100843f272
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2017-02-02 19:03:58 +0100

    AVX512 VPOPCNT: slightly better tail processing

--- popcnt-avx512-vpopcnt.cpp
+++ popcnt-avx512-vpopcnt.cpp
@@ -16,9 +16,9 @@ __m512i _mm512_popcnt_epi64(const __m512i& val) {
 uint64_t avx512_vpopcnt(const uint8_t* data, const size_t size) {
     
     const size_t chunks = size / 64;
-    const size_t tail   = size % 64;
 
     uint8_t* ptr = const_cast<uint8_t*>(data);
+    const uint8_t* end = ptr + size;
 
     // count using AVX512 registers
     __m512i accumulator = _mm512_setzero_si512();
@@ -40,8 +40,13 @@ uint64_t avx512_vpopcnt(const uint8_t* data, const size_t size) {
         total += tmp[i];
     }
 
-    // popcount the tail (an unoptimal way, we could count 64-bit values if possible)
-    for (size_t i=0; i < tail; i++) {
+    // popcount the tail
+    while (ptr + 8 < end) {
+        total += _mm_popcnt_u64(*reinterpret_cast<const uint64_t*>(ptr));
+        ptr += 8;
+    }
+
+    while (ptr < end) {
         total += lookup8bit[*ptr++];
     }
 
